62c087981d74a1bccf0ff20f8eb4fba37bc0ebb6
richard-dinh <richarddinh@gmail.com>
2020-02-29 13:57:11 -0800

    set up react views and htlm boiler

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const {join} = require('path')
 
 
 const app = express()
-
+app.engine('jsx', require('express-react-views').createEngine())
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'jsx')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(join(__dirname, 'public')))
