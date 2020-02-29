d634e05171095ce46ef7a9c42cb8a57e28762900
richard-dinh <richarddinh@gmail.com>
2020-02-29 14:47:58 -0800

    set up initial views

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ app.use(express.static(join(__dirname, 'public')))
 
 app.use(require('./routes'))
 
+// route for homepage
+app.get('/', (request, response) => {
+  response.render('index')
+})
+
 app.listen(process.env.PORT || 3000)
