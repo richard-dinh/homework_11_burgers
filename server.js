a79b4fa5bb1a3707951382189d30f695ef5e083d
richard-dinh <richarddinh@gmail.com>
2020-02-28 17:52:31 -0800

    wrote routes and connected to server.js

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(join(__dirname, 'public')))
 
+app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000)
