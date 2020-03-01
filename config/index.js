e0d525597184844bc3d062c47642fbc2ba1b17ad
richard-dinh <richarddinh@gmail.com>
2020-02-29 21:14:43 -0800

    added JWASDB_URL to db connection

--- config/index.js
+++ config/index.js
@@ -1,11 +1,20 @@
 const {createConnection} = require('mysql2')
 
-const connection = createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-})
+const connection
+
+if (process.env.JAWSDB_URL) {
+  connection = createConnection(proccess.env.JAWSDB_URL)
+}else{
+  connection = createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  })
+}
+
+
+
 
 module.exports = connection
