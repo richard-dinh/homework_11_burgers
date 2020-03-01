bc1925f4dba12a1dd024c237853848aa2176053e
richard-dinh <richarddinh@gmail.com>
2020-02-29 21:33:57 -0800

    fixed heroku db connection

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const {createConnection} = require('mysql2')
 const connection
 
 if (process.env.JAWSDB_URL) {
-  connection = createConnection(proccess.env.JAWSDB_URL)
+  connection = createConnection(process.env.JAWSDB_URL)
 }else{
   connection = createConnection({
     host: 'localhost',
